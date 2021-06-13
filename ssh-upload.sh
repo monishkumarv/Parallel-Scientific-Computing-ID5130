64eb8c70d5c77f05b59d7090ab08d295b471a34e
Monish Kumar <49451772+monishkumarv@users.noreply.github.com>
2021-06-13 21:13:49 +0530

    Update ssh-upload.sh

--- ssh-upload.sh
+++ ssh-upload.sh
@@ -4,5 +4,5 @@
 # scp -r openacc/ ce18b118@aqua.iitm.ac.in:scratch/
 
 # Upload remote
-sshpass -p "Monish_1430" scp -o StrictHostKeyChecking=no -r course-project/ ce18b118@aqua.iitm.ac.in:scratch/
+sshpass -p "<password>" scp -o StrictHostKeyChecking=no -r course-project/ ce18b118@aqua.iitm.ac.in:scratch/
 echo "File upload successful!"
